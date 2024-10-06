431697ceca649166998dc2d2700ee895d7935c29
Dimpi173 <122535327+Dimpi173@users.noreply.github.com>
2024-10-06 18:00:56 +0530

    Add files via upload

---
+++ higher_lower_game.py
@@ -0,0 +1,60 @@
+import high_low_logo
+import question
+import random
+import os
+
+
+def option_list():
+    if choice_1 == choice_2:
+        option_list()
+    else:
+        return choice_1, choice_2
+
+
+def display_option(choice):
+    name = choice["name"]
+    description = choice["description"]
+    country = choice["country"]
+    return f": {name}, {description}, {country}"
+
+
+def check_answer(guess, follower_1, follower_2):
+    if follower_1 < follower_2:
+        if guess == "first":
+            return False
+        else:
+            return True
+    else:
+        if guess == "first":
+            return True
+        else:
+            return False
+
+
+choice_2 = random.choice(question.data)
+win_score = 0
+end_game = False
+while not end_game:
+    choice_1 = choice_2
+    choice_2 = random.choice(question.data)
+    print(high_low_logo.first)
+    print("\nGuess who has more followers on Instagram.....  \n")
+    option = option_list()
+    print(f"Option 1 {display_option(option[0])}\n")
+    print(high_low_logo.vs)
+    print(f"Option 2 {display_option(option[1])}\n")
+    follower1 = option[0]["follower_count"]
+    follower2 = option[1]["follower_count"]
+    user_guess = input("For option first type 'first' or type 'second' : ").lower()
+    while user_guess not in ["first", "second"]:
+        user_guess = input("For option first type 'first' or type 'second' : ").lower()
+    correct_ans = check_answer(user_guess, follower1, follower2)
+    os.system('cls')
+    if correct_ans:
+        win_score += 1
+        print(f"\nWON.... Your Score is {win_score}")
+    else:
+        print(f"\nLOSE.... Your Score is {win_score}")
+        end_game = True
+
+print("Good Bye......")
---
+++ question.py
@@ -0,0 +1,86 @@
+data = [
+    {
+        "name": "ISRO",
+        "follower_count": 546000,
+        "description": "Indian Space Research Organization",
+        "country": "India"
+    },
+    {
+        "name": "NASA",
+        "follower_count": 97100000,
+        "description": "National Aeronautics and Space Administration",
+        "country": "United State"
+    },
+    {
+        "name": "CNSA",
+        "follower_count": 8776,
+        "description": "China National Space Administration",
+        "country": "China"
+    },
+    {
+        "name": "CSA",
+        "follower_count": 553000,
+        "description": "Canadian Space Agency",
+        "country": "Canada"
+    },
+    {
+        "name": "SCIAM",
+        "follower_count": 461000,
+        "description": "Scientific American",
+        "country": "America"
+    },
+    {
+        "name": "esa",
+        "follower_count": 3000000,
+        "description": "European Space Agency",
+        "country": "Europe"
+    },
+    {
+        "name": "DLR",
+        "follower_count": 147000,
+        "description": "Deutsches Zentrum Fur Luft- u. Raumfahrt",
+        "country": "Germany"
+    },
+    {
+        "name": "KASA",
+        "follower_count": 9999,
+        "description": "Korea Aerospace Administration",
+        "country": "Korea"
+    },
+    {
+        "name": "CNES",
+        "follower_count": 169000,
+        "description": "Centre National d'Etudes Spatiales",
+        "country": "France"
+    },
+    {
+        "name": "UAESA",
+        "follower_count": 54200,
+        "description": "UAE Space Agency",
+        "country": "United Arab Emirates"
+    },
+    {
+        "name": "JAXA",
+        "follower_count": 251000,
+        "description": "Japan Aerospace Exploration Agency",
+        "country": "Japan"
+    },
+    {
+        "name": "ASI",
+        "follower_count": 82600,
+        "description": "Agenzia Spaziale Italiana",
+        "country": "Italy"
+    },
+    {
+        "name": "Roscosmos",
+        "follower_count": 753000,
+        "description": "Russian Federal Space Agency",
+        "country": "Russia"
+    },
+    {
+        "name": "ISS",
+        "follower_count": 8800000,
+        "description": "International Space Station",
+        "country": "International"
+    }
+]
